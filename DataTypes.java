48be2f518857acee29d2051f54b222ac3e7ef7b8
Savio Shamoon <savio.shamoon@mrjaffesclass.com>
2022-09-16 11:03:32 -0700

    Initial commit

--- DataTypes.java
+++ DataTypes.java
@@ -5,9 +5,11 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class RenameThisClass
+public class DataTypes
 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int i;
+        float f = 2.5f;
+        boolean flag = true;
     }
 }
